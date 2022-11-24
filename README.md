fbcab50d44f87873e30066add3d5200c056f576c
Zaid Ansari <99890019+zaidansari42@users.noreply.github.com>
2022-11-24 20:07:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# 🚘 Responsive Car Rentals & Sales Website
+
+## [Watch it on youtube](https://youtu.be/INqnUCQ77-k)
+
 ### 🚘 Responsive Car Rentals & Sales Website
 
 - Responsive Car website Using HTML CSS & JavaScript
@@ -5,5 +9,7 @@
 - Smooth scrolling in each section.
 - Developed first with the Mobile First methodology, then for desktop.
 - Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+💙 Join the channel to see more videos like this. [JustPerfect Designs](https://www.youtube.com/c/Justperfectdesigns)
 
 ![preview img](/preview.jpg)
--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,12 @@ const removeMenu = function (e) {
   nav__menu.classList.remove('show-menu');
 };
 
+// Adding Listener to Navigation Links
+
+document
+  .querySelectorAll('.nav__link')
+  .forEach((el) => el.addEventListener('click', removeMenu));
+
 // SHOWING MENU ON CLICK
 nav__toggle.addEventListener('click', showMenu);
 
@@ -58,6 +64,59 @@ var mixerProduct = mixitup('.product-cards', {
   },
 });
 
-/*=============== SHOW SCROLL UP ===============*/
+/* SHOW SCROLL UP */
 
-// SCROLL REVEAL JS =================================
+const scrollUp = function () {
+  const scrollUp = document.getElementById('scrollUp');
+
+  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll-up');
+  else scrollUp.classList.remove('show-scroll-up');
+};
+
+window.addEventListener('scroll', scrollUp);
+
+/* SCROLL REVEAL JS */
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2500,
+  delay: 200,
+});
+
+sr.reveal('.hero-subtitle');
+sr.reveal('.hero-title', { delay: 800, origin: 'left' });
+sr.reveal('.hero-description', { delay: 1000, origin: 'left' });
+sr.reveal('.hero-button', { delay: 1200, origin: 'right' });
+sr.reveal('.statics-cars', { delay: 1200, origin: 'right' });
+sr.reveal('.statics-customers', { delay: 1600, origin: 'left' });
+sr.reveal('.statics-line', { delay: 0, distance: '400px' });
+sr.reveal('.hero-image', { origin: 'bottom' });
+
+sr.reveal('.about');
+sr.reveal('.about__container', { origin: 'right', delay: 800 });
+sr.reveal('.about-image', { origin: 'left', delay: 800 });
+sr.reveal('.card', { origin: 'bottom', delay: 800, interval: 1000 });
+
+sr.reveal('.brands-logo', { delay: 400, interval: 200 });
+
+sr.reveal('.why', { delay: 400, origin: 'left', interval: 200 });
+
+sr.reveal('.newsletter__container', { origin: 'bottom' });
+sr.reveal('.newsletter-title', {
+  origin: 'left',
+  distance: '500px',
+  delay: 400,
+});
+
+sr.reveal('.newsletter-description', { origin: 'bottom', delay: 1500 });
+sr.reveal('.newsletter-input', { origin: 'bottom', delay: 1500 });
+
+sr.reveal('.products__container');
+sr.reveal('.product-group', { interval: 200, origin: 'left' });
+sr.reveal('.product-card', { interval: 300, distance: '40px', duration: 1000 });
+
+sr.reveal('.app-image', { origin: 'right' });
+sr.reveal('.app__container', { origin: 'left', delay: 800 });
+
+sr.reveal('.app', { interval: 400 });
